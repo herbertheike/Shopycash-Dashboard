95b83f90957d600628cf894036bb650c106a630c
herberth eike <herbertheike.ti@gmail.com>
2021-05-14 18:31:59 -0300

    delivery flow

--- src/dashboard/loja/orders.js
+++ src/dashboard/loja/orders.js
@@ -91,10 +91,8 @@ class CadastroCat extends React.Component {
 
   cartDetail = async () => {
     const id = this.state.cartid;
-    const userId = this.state.userid
     console.log(id, "new id");
-    console.log(userId, "new id");
-    await fetch("https://api-shopycash1.herokuapp.com/cart/" +userId+"/"+ id, {
+    await fetch("https://api-shopycash1.herokuapp.com/store/viewdelivery/" +localStorage.getItem("@lojaid")+"/"+ this.state.cartid, {
       method: "GET",
       headers: {
         Accept: "application/json",
@@ -102,7 +100,7 @@ class CadastroCat extends React.Component {
         Authorization: "Bearer " + localStorage.getItem("@token"),
       },
     }).then((res) =>res.json())
-      .then((res) => this.setState({cartitens: res[0].cartitens}))
+      .then((res) => this.setState({cartitens: res[0].produtos}))
       .catch((error) => console.log(error))
       .finally(() => this.setState({ isLoaded: false }));
 
@@ -220,14 +218,18 @@ const order1 = this.state.orderstoprocess;
                   <tr>
                   <td style={{alignItems:'center', textAlign:'center', justifyContent:'center'}}>{item.nome}</td>
                   <td style={{alignItems:'center', textAlign:'center', justifyContent:'center'}}>{item.cpf}</td>
-                  <td style={{alignItems:'center', textAlign:'center', justifyContent:'center'}}>{item.address}</td>
+                  <td style={{alignItems:'center', textAlign:'center', justifyContent:'center'}}>
+                   <p> {item.deliveryadress.logradouro+", "+item.deliveryadress.numero+","+
+                    item.deliveryadress.bairro+", "+item.deliveryadress.cidade+"/"+
+                    item.deliveryadress.estado+" \n"+item.deliveryadress.referencia
+                    }</p></td>
                   <td style={{alignItems:'center', textAlign:'center', justifyContent:'center'}}>{item.paymentmethod}</td>
                   <td style={{alignItems:'center', textAlign:'center', justifyContent:'center'}}>{item.shippingmethod}</td>
                   <td style={{alignItems:'center', textAlign:'center', justifyContent:'center'}}>{item.shippingprice}</td>
                   <td style={{alignItems:'center', textAlign:'center', justifyContent:'center'}}>{item.total}</td>
-                  <td style={{alignItems:'center', textAlign:'center', justifyContent:'center'}}>{item.troco}</td>
+                  <td style={{alignItems:'center', textAlign:'center', justifyContent:'center'}}>{item.change}</td>
                     <td>
-                  <Button onClick={() => this.openModal(item.cartid, item.userid)}>
+                  <Button onClick={() => this.openModal(item._id, item.userid)}>
                     <Icon name="x" />
                     Despachar pedido
                   </Button>
@@ -244,26 +246,88 @@ const order1 = this.state.orderstoprocess;
             style={this.state.customStyles}
           >
             <div>
-              {this.state.cartitens.map((item)=>{
-                return (
-                    <div>
-                      <label>
-                        {item._id}
-                      </label>
-                      <label>
-                        {item.produto}
-                      </label>
+            {this.state.orderstoprocess.map((item) => {
+                  return (
+                  <div>
+                    <Title>
+                    PEDIDO Nº: {item._id}
+                    </Title>
+                    <Label>Dados do Cliente</Label>
+                    <div style={{display:'flex',flexDirection:'column' ,alignItems:'flex-start',
+                     justifyContent:'space-between'}}>
+                       
+                       <div style={{display:'flex',flexDirection:'row', padding:10}}>
+                        <span style={{ padding:10}}>Nome:</span> <h6 style={{ padding:10}}>{item.nome}</h6> 
+                        <span style={{ padding:10}}>CPF:</span> <h6 style={{ padding:10}}>{item.cpf}</h6>
+                        <span style={{ padding:10}}>Telefone:</span> <h6 style={{ padding:10}}>{item.cpf}</h6>
+                        <span style={{ padding:10}}>Email:</span> <h6 style={{ padding:10}}>{item.cpf}</h6>
+                     </div>
 
-                    <label>
-                      x{item.qty}
-                    </label>
-                    <label>
-                      R$'{item.unitPrice}
-                    </label>
-                  </div>                 
-                );
-              })}
+                    <div style={{display:'flex',flexDirection:'row' ,
+                     justifyContent:'space-between'}}>
+                       <span>Endereço</span>
+                       <h6>{item.deliveryadress.logradouro}, {item.deliveryadress.numero}, 
+                    {item.deliveryadress.bairro}, {item.deliveryadress.cidade}/
+                    {item.deliveryadress.estado} - {item.deliveryadress.referencia}</h6>
+                    </div>
+                    </div>
+                  <table style={{alignItems:'center', justifyContent:'center', width:'100%'}} > 
+                  <thead style={{alignItems:'center', justifyContent:'center'}}>
+                    <tr>
+                      <th style={{alignItems:'center', textAlign:'center', justifyContent:'center', width:"auto", padding:3}}>SKU</th>
+                      <th style={{alignItems:'center', textAlign:'center', justifyContent:'center', width:"auto", padding:3}}>Produto</th>
+                      <th style={{alignItems:'center', textAlign:'center', justifyContent:'center', width:"auto", padding:3}}>Categoria</th>
+                      <th style={{alignItems:'center', textAlign:'center', justifyContent:'center', width:"auto", padding:3}}>Preço unitario</th>
+                      <th style={{alignItems:'center', textAlign:'center', justifyContent:'center', width:"auto", padding:3}}>Quantidade</th>
+                    </tr>
+                  </thead>
+                  {this.state.cartitens.map((prod)=>{
+                      return (
+                  <tbody style={{backgroundColor:'#53aaa8', height:100, overflowY: 'auto', padding: 5}}>
+                    <tr>
+                    <td style={{alignItems:'center', textAlign:'center', justifyContent:'center', fontSize:10, borderWidth:1}}>{prod.produtoid}</td>
+                    <td style={{alignItems:'center', textAlign:'center', justifyContent:'center', fontSize:10, borderWidth:1}}>{prod.produto}</td>
+                    <td style={{alignItems:'center', textAlign:'center', justifyContent:'center', fontSize:10, borderWidth:1}}>{prod.categoria}</td>
+                    <td style={{alignItems:'center', textAlign:'center', justifyContent:'center', fontSize:10, borderWidth:1}}>{prod.unitPrice}</td>
+                    <td style={{alignItems:'center', textAlign:'center', justifyContent:'center', fontSize:10, borderWidth:1}}>{prod.qty}</td>
+                    </tr>
+                  </tbody> 
+                    );
+                })}                    
+                  </table>
+                  <Label>Dados do pagamento</Label>
+                    <div style={{display:'flex',flexDirection:'row' ,alignItems:'flex-start',
+                     justifyContent:'flex-start'}}>
+                      
+                      <div style={{display:'flex',flexDirection:'column',padding: 10,alignItems:'flex-start',
+                     justifyContent:'space-between'}}>
+                      <span style={{fontSize: 18, fontStyle: 'italic'}}>Forma de pagamento: </span><h6 style={{fontSize: 20, fontStyle: 'normal'}}>{item.paymentmethod}</h6>
+                      <br />
+                      </div>
+                      
+                    <div style={{display:'flex',flexDirection:'column',padding: 10,alignItems:'flex-start',
+                     justifyContent:'space-between'}}>
+                    <span style={{fontSize: 18, fontStyle: 'italic'}}>Subtotal: </span> <h6 style={{fontSize: 20, fontStyle: 'normal'}}>R${item.subTotal}</h6><br />
+                    <span style={{fontSize: 18, fontStyle: 'italic'}}>Taxa de entrega: </span> <h6 style={{fontSize: 20, fontStyle: 'normal'}}>R${item.shippingprice}</h6><br />
+                    <span style={{fontSize: 18, fontStyle: 'italic'}}>Valor total: </span> <h6 style={{fontSize: 20, fontStyle: 'normal'}}>R${item.total}</h6><br />
+                    </div>
+                    <div style={{display:'flex',flexDirection:'column',padding: 10,alignItems:'flex-start',
+                     justifyContent:'space-between'}}>
+                      <span style={{fontSize: 18, fontStyle: 'italic'}}>Troco: </span> <h6 style={{fontSize: 20, fontStyle: 'normal'}}>R${item.change}</h6>
+                      <br />
+                      </div>
+                    </div>
+                  <button style={{alignItems:'center', textAlign:'center', justifyContent:'center', fontSize:10}}
+                    onClick={() => console.log("Pedido despachado")}>
+                      <Icon name="x" />
+                      Despachar pedido
+                    </button>
+                  </div>   
+                  );
+                })}
+                  
             </div>
+            
             <button onClick={this.closeModal}>close</button>
           </Modal>
                 </div>
